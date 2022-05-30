d98cf5b6b2b20d241c7d1e913ebe88764d57bf42
Jaap Wiering <github.com@fanagalo.nl>
2022-05-30 16:35:33 +0200

    Update functions.php
    change initialise child theme

--- functions.php
+++ functions.php
@@ -2,13 +2,11 @@
 
 /* initialise child theme */
 
-add_action(
-  'wp_enqueue_scripts',
-  function () {
-     wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/style.css');
-  },
-  10
-);
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
 
 
 /* Declaration of custom post type "glossary" */
