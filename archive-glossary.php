c4b4bc9402209087270f6c86bc7c9ac110ff279e
Jaap Wiering <github.com@fanagalo.nl>
2022-06-10 15:10:25 +0200

    another try

--- archive-glossary.php
+++ archive-glossary.php
@@ -51,16 +51,16 @@ get_header();
 								'<td><a href="' . $link .  '">' .  $title  . '</a></td>' .
 								'<td>';
 
-						if (!empty($terms)) :
+						// if (!empty($terms)) :
 							foreach ($terms as $term) {
 								echo '<a href="' . get_term_link($term) .  '">' .  $term->name  . '</a>' ;
 							}
-						endif;
+						// endif;
 
 						// var_dump($terms);
 							
 						echo '</td><td>';
-						get_the_content();
+							get_the_content();
 						echo '</td></tr>';
 				endwhile;
 
