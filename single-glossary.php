1230d46892ad3f29f66cf1989d892c7ae2e27eda
Jaap Wiering <github.com@fanagalo.nl>
2022-09-08 15:37:16 +0200

    sidebar colors

--- single-glossary.php
+++ single-glossary.php
@@ -18,6 +18,37 @@ get_header();
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
 		endif;
+
+
+		$post_title = get_the_title();
+	
+		if( has_term( 'category', 'glossary-category' ) ) {
+			echo 'ik ben category';
+		}
+		echo 'blabla' . $post_title;
+
+		/*
+
+		second query
+
+		if post has gloss_cat 18 
+
+		for post_title = 
+		if post_title = glossary_cat
+
+		show all items in glossary_cat
+
+
+
+
+
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'template-parts/content', 'glossary-table' );
+		endwhile; 
+
+		*/
+
 	?>
 
 </main>
